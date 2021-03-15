ac47f456af11331555f17cd0a678f3ec9d62e8ef
ghirailghiro <50736672+ghirailghiro@users.noreply.github.com>
2021-03-15 10:59:10 +0100

    Update consegna_1.py

--- consegna_1.py
+++ consegna_1.py
@@ -12,3 +12,16 @@ def pretty_print(KeyWord, Value):
 worldFootball = pd.read_csv("results.csv")
 
 Team = "Italy"
+
+#Creazione DataFrame partite di Team
+TeamMatches = worldFootball.loc[(worldFootball.home_team == Team) | (worldFootball.away_team == Team)].copy()
+
+#Andremo ad inserire una colonna con le segueti KeyWords: W = (vittoria/Win), L = (Sconfitta/Lose) e D (Pareggio/Draft)
+
+match_results=[]
+
+for index,row in TeamMatches.iterrows():
+
+#aggiungiamo la colonna con W,L e D al DataFrame
+newColumn = pd.Series(match_results)
+TeamMatches['result'] = newColumn.values
