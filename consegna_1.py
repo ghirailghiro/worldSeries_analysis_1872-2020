847e1489079b4d1da35b7c4e042379d052fb120f
ghirailghiro <50736672+ghirailghiro@users.noreply.github.com>
2021-03-15 14:04:21 +0100

    refactoring

--- consegna_1.py
+++ consegna_1.py
@@ -2,7 +2,7 @@ import pandas as pd
 from itertools import islice
 import matplotlib.pyplot as plt
 
-def team_Indicators(DataFrame,Team):
+def team_Indicators(DataFrame,Team,printData,plot):
     TeamMatches = DataFrame.loc[(DataFrame.home_team == Team) | (DataFrame.away_team == Team)].copy()
 
     wins = len(TeamMatches[((TeamMatches.home_team == Team) & (TeamMatches.home_score > TeamMatches.away_score)) | ((TeamMatches.away_team == Team) & (TeamMatches.home_score < TeamMatches.away_score))].index)
@@ -17,10 +17,27 @@ def team_Indicators(DataFrame,Team):
     goals_home.columns = ['date','goals_scored','goals_conceded']
     goals_away.columns = ['date','goals_scored','goals_conceded']
     frames_goals = [goals_home,goals_away]
-
     goals= pd.concat(frames_goals).sort_index()
+    scored_mean = goals['goals_scored'].mean()
+    conceded_mean = goals['goals_conceded'].mean()
+    if(printData):
+        print("Wins : " + str(wins))
+        print("Loses : " + str(loses))
+        print("Drafts : " + str(drafts))
+        print("Sum of W+L+D : " + str(wins+loses+drafts))
+        print("Average scored goals : "+ str(scored_mean))
+        print("Average conceded goals : "+ str(conceded_mean))
+    
+    if(plot):
+        goals['goals_difference']=goals['goals_scored']-goals['goals_conceded']
+        goals.plot(kind = "line", x="date", y="goals_scored", label="Goal Scored")
+        goals.plot(kind = "line", x="date", y="goals_conceded", label="Goal Conceded")
+        goals.plot(kind = "line", x="date", y="goals_difference", label="Goal Difference")
+        plt.show()
 
-    return wins,loses,drafts,goals['goals_scored'].mean(),goals['goals_conceded'].mean()
+    del TeamMatches
+
+    return wins,loses,drafts,scored_mean,conceded_mean
 
 def pretty_print(KeyWord, Value):
     print("--------------------------------------------------------------------------------------------------------------------------------------------------")
@@ -32,59 +49,9 @@ worldFootball = pd.read_csv("results.csv")
 
 Team = "Italy"
 
-print(team_Indicators(worldFootball,Team))
-#Creazione DataFrame partite di Team
-TeamMatches = worldFootball.loc[(worldFootball.home_team == Team) | (worldFootball.away_team == Team)].copy()
-numberOfMatches = len(TeamMatches.index)
-
-print("Number of Matches : " + str(numberOfMatches))
-
-wins = len(TeamMatches[((TeamMatches.home_team == Team) & (TeamMatches.home_score > TeamMatches.away_score)) | ((TeamMatches.away_team == Team) & (TeamMatches.home_score < TeamMatches.away_score))].index)
-
-loses = len(TeamMatches[((TeamMatches.home_team == Team) & (TeamMatches.home_score < TeamMatches.away_score)) | ((TeamMatches.away_team == Team) & (TeamMatches.home_score > TeamMatches.away_score))].index)
-
-drafts = len(TeamMatches[TeamMatches.home_score == TeamMatches.away_score].index)
-
-print("Wins : " + str(wins))
-print("Loses : " + str(loses))
-print("Drafts : " + str(drafts))
-print("Sum of W+L+D : " + str(wins+loses+drafts))
-
-goals_home = TeamMatches[['date','home_score','away_score']][(TeamMatches.home_team == Team)]
-goals_away = TeamMatches[['date','away_score','home_score']][(TeamMatches.away_team == Team)]
-
-goals_home.columns = ['date','goals_scored','goals_conceded']
-goals_away.columns = ['date','goals_scored','goals_conceded']
-
-#print(goals_home)
-#print(goals_away)
-
-frames_goals = [goals_home,goals_away]
-
-goals= pd.concat(frames_goals).sort_index()
-
-#print(goals)
-
-averageScored = goals['goals_scored'].mean()
-averageConceded = goals['goals_conceded'].mean()
-
-goals['goals_difference']=goals['goals_scored']-goals['goals_conceded']
-
-print("Average scored goals : "+ str(averageScored))
-print("Average conceded goals : "+ str(averageConceded))
-
-print(goals)
-
-#Ploting the results
-'''
-goals.plot(kind = "line", x="date", y="goals_scored", label="Goal Scored")
-goals.plot(kind = "line", x="date", y="goals_conceded", label="Goal Conceded")
-goals.plot(kind = "line", x="date", y="goals_difference", label="Goal Difference")
-
-plt.show()
-'''
-
+print(team_Indicators(worldFootball,Team,False,False))
 #Andremo ad inserire una colonna con le segueti KeyWords: W = (vittoria/Win), L = (Sconfitta/Lose) e D (Pareggio/Draft)
+
 '''
 match_results=[]
 
