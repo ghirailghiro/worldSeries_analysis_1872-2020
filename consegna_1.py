86d5faae1c00cd6e1df94bddd35980e117aed85b
ghirailghiro <50736672+ghirailghiro@users.noreply.github.com>
2021-03-15 11:22:46 +0100

    update with masks

--- consegna_1.py
+++ consegna_1.py
@@ -16,8 +16,21 @@ Team = "Italy"
 #Creazione DataFrame partite di Team
 TeamMatches = worldFootball.loc[(worldFootball.home_team == Team) | (worldFootball.away_team == Team)].copy()
 
-#Andremo ad inserire una colonna con le segueti KeyWords: W = (vittoria/Win), L = (Sconfitta/Lose) e D (Pareggio/Draft)
+print("Number of Matches : " + str(len(TeamMatches.index)))
 
+wins = len(TeamMatches[((TeamMatches.home_team == Team) & (TeamMatches.home_score > TeamMatches.away_score)) | ((TeamMatches.away_team == Team) & (TeamMatches.home_score < TeamMatches.away_score))].index)
+
+loses = len(TeamMatches[((TeamMatches.home_team == Team) & (TeamMatches.home_score < TeamMatches.away_score)) | ((TeamMatches.away_team == Team) & (TeamMatches.home_score > TeamMatches.away_score))].index)
+
+drafts = len(TeamMatches[TeamMatches.home_score == TeamMatches.away_score].index)
+
+
+print("Wins : " + str(wins))
+print("Loses : " + str(loses))
+print("Drafts : " + str(drafts))
+print("Sum of W+L+D : " + str(wins+loses+drafts))
+#Andremo ad inserire una colonna con le segueti KeyWords: W = (vittoria/Win), L = (Sconfitta/Lose) e D (Pareggio/Draft)
+'''
 match_results=[]
 
 for index,row in TeamMatches.iterrows():
@@ -35,4 +48,4 @@ for index,row in TeamMatches.iterrows():
 
 #aggiungiamo la colonna con W,L e D al DataFrame
 newColumn = pd.Series(match_results)
-TeamMatches['result'] = newColumn.values
+TeamMatches['result'] = newColumn.values'''
